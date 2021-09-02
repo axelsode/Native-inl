5e69cb644ff60db459ebe0deedf6a97eab872552
Axl <axel.soderberg1996@gmail.com>
2021-09-02 22:49:05 +0200

    finnal push!

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,23 +1,29 @@
 import { useState, useEffect } from "react";
 import yelp from "../api/yelp";
+import data from "../../response-data-export.json"
+
 
 export default () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async(searchTerm) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'Stockholm'
-                }
-            });
-            setResults(response.data.businesses);
-        } catch (err) {
-            setErrorMessage('Something went wrong')
-        }
+        console.log('test')
+        setResults(data.businesses);
+        // try {
+        //     const response = await yelp.get('/search', {
+                
+        //         params: {
+        //             limit: 50,
+        //             term: searchTerm,
+        //             location: 'Stockholm'
+        //         }
+        //     });
+        //     consol.log('test2')
+        //     setResults(response.data.businesses);
+        // } catch (err) {
+        //     setErrorMessage('Something went wrong')
+        // }
     };
 
     useEffect(() => {
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
+    
     const[term,setTerm] = useState('');
     const [searchApi, results, errorMessage] = useResults();
       
@@ -16,18 +17,32 @@ const SearchScreen = () => {
         })
     }
     return (
-        <View>
+        <>
             <SearchBar 
             term={term} 
             onTermChange={setTerm}
             onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
-            <Text>We have found {results.length} results </Text>
-            <ResultsList results={filterResultsByPrice('$')} title="Cost Effective"/>
-            <ResultsList results={filterResultsByPrice('$$')} title="Bit pricier"/>
-            <ResultsList results={filterResultsByPrice('$$$')} title="Big spender"/>
-        </View>
+            
+            <ScrollView>
+                <ResultsList 
+                results={filterResultsByPrice('$')} 
+                title="Cost Effective"
+                
+                />
+                <ResultsList 
+               results={filterResultsByPrice('$$')} 
+                title="Bit pricier"
+                
+                />
+                <ResultsList 
+                results={filterResultsByPrice('$$$')} 
+                title="Big spender"
+                
+                />
+            </ScrollView>
+        </>
     )
 };
 
